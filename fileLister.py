ae8689ca5912fb1aeb436b91869686b501470721
frank_qcd_qk <frank_chude.qian@outlook.com>
2019-07-22 14:16:16 -0400

    Update fileLister.py

--- fileLister.py
+++ fileLister.py
@@ -1,1 +1,11 @@
+import glob, os
+# Current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+print(current_dir)
 
+# Create and/or truncate train.txt and test.txt
+file_train = open('summary.txt', 'w')  
+for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.png")):  
+    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+    print(title)
+    file_train.write(current_dir + "/" + title + '.jpg' + "\n")
